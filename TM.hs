50ea347dd4b83addc1115dd07158e6fd8bd13953
Jade Kessinger <jadekessinger13@gmail.com>
2024-10-22 19:39:01 -0700

    Cleaned up indentations

--- TM.hs
+++ TM.hs
@@ -17,21 +17,21 @@ import qualified Data.Map as Map
 
 machine = Machine {
     config = Config Tape {left = ["0", "1", "0", "1", "1"], 
-                           right = [], 
-                           tapeHead = "_"} "0", 
+                          right = [], 
+                          tapeHead = "_"} "0", 
     states = Map.fromList [
-    ("0", Map.fromList [
-        ("0", Transition {write = "0", move = Lt, goto = "Halt"}), 
-        ("1", Transition {write = "1", move = Rt, goto = "Halt"}),
-        ("_", Transition {write = "_", move = Lt, goto = "1"})]),
-    ("1", Map.fromList [
-        ("0", Transition {write = "1", move = Lt, goto = "1"}), 
-        ("1", Transition {write = "0", move = Lt, goto = "1"}),
-        ("_", Transition {write = "_", move = Rt, goto = "2"})]),
-    ("2", Map.fromList [
-        ("0", Transition {write = "0", move = Rt, goto = "2"}),
-        ("1", Transition {write = "1", move = Rt, goto = "2"}),
-        ("_", Transition {write = "_", move = Rt, goto = "Halt"})])]}
+        ("0", Map.fromList [
+            ("0", Transition {write = "0", move = Lt, goto = "Halt"}), 
+            ("1", Transition {write = "1", move = Rt, goto = "Halt"}),
+            ("_", Transition {write = "_", move = Lt, goto = "1"})]),
+        ("1", Map.fromList [
+            ("0", Transition {write = "1", move = Lt, goto = "1"}), 
+            ("1", Transition {write = "0", move = Lt, goto = "1"}),
+            ("_", Transition {write = "_", move = Rt, goto = "2"})]),
+        ("2", Map.fromList [
+            ("0", Transition {write = "0", move = Rt, goto = "2"}),
+            ("1", Transition {write = "1", move = Rt, goto = "2"}),
+            ("_", Transition {write = "_", move = Rt, goto = "Halt"})])]}
 
 resultConfig = simulate machine 
 resultString = stringFormat resultConfig
